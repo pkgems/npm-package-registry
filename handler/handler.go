ca6e8abdb064b3680ca028782977ba9d43436f93
 Ilya Atamas <stackdumper@gmail.com>
2019-05-02 18:30:29 +0300

    feat: add DynamicHandler

--- handler/handler.go
+++ handler/handler.go
@@ -11,7 +11,56 @@ import (
 func Handler(core *registry.Core) http.Handler {
 	router := gin.Default()
 
-	PackageRoutes(router, core)
+	router.Use(coreMiddleware(core))
+
+	PackageRoutes(router)
 
 	return router
+}
+
+// DynamicHandler provides http interface for Registry
+// allowing for dynamic core setup
+func DynamicHandler(getCore func() *registry.Core) http.Handler {
+	router := gin.Default()
+
+	router.Use(dynamicCoreMiddleware(getCore))
+
+	PackageRoutes(router)
+
+	return router
+}
+
+func coreMiddleware(core *registry.Core) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("core", core)
+
+		c.Next()
+	}
+}
+
+func dynamicCoreMiddleware(getCore func() *registry.Core) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("core", getCore())
+
+		c.Next()
+	}
+}
+
+type HandlerFunc func(core *registry.Core, c *gin.Context) (int, error)
+
+func handlerWrapper(handler HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		core := c.MustGet("core").(*registry.Core)
+
+		status, err := handler(core, c)
+		if status != 0 {
+			if err != nil {
+				c.JSON(status, gin.H{
+					"error": err.Error(),
+				})
+			} else {
+				c.Status(status)
+			}
+		}
+	}
 }
--- handler/package.go
+++ handler/package.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -9,79 +10,62 @@ import (
 )
 
 // PackageRoutes provides routes for interactions with packages
-func PackageRoutes(router *gin.Engine, core *registry.Core) {
-	router.PUT("/:name", publishPackageHandler(core))
-	router.GET("/:name", getPackageHandler(core))
-	router.GET("/:name/-/:version", getPackageTarballHandler(core))
+func PackageRoutes(router *gin.Engine) {
+	router.PUT("/:name", handlerWrapper(publishPackageHandler))
+	router.GET("/:name", handlerWrapper(getPackageHandler))
+	router.GET("/:name/-/:version", handlerWrapper(getPackageTarballHandler))
 }
 
-func publishPackageHandler(core *registry.Core) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var user = registry.User{
-			Username: "anonymous",
-			Password: "anonymous",
-		}
-
-		data, err := c.GetRawData()
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		err = core.PublishPackage(string(data), user, c.Request.URL.Scheme+c.Request.Host)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
+func publishPackageHandler(core *registry.Core, c *gin.Context) (int, error) {
+	var user = registry.User{
+		Username: "anonymous",
+		Password: "anonymous",
 	}
+
+	data, err := c.GetRawData()
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	err = core.PublishPackage(string(data), user, c.Request.URL.Scheme+c.Request.Host)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	return 200, nil
 }
 
-func getPackageHandler(core *registry.Core) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var name = c.Param("name")
+func getPackageHandler(core *registry.Core, c *gin.Context) (int, error) {
+	var name = c.Param("name")
 
-		var pkg, err = core.GetPackage(name)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		if pkg == "" {
-			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
-				"error": "not found",
-			})
-		} else {
-			c.Data(http.StatusOK, "application/json", []byte(pkg))
-		}
+	var pkg, err = core.GetPackage(name)
+	if err != nil {
+		return http.StatusInternalServerError, err
 	}
+
+	if pkg == "" {
+		return http.StatusNotFound, fmt.Errorf("package not found")
+	}
+
+	c.Data(http.StatusOK, "application/json", []byte(pkg))
+	return 0, nil
 }
 
-func getPackageTarballHandler(core *registry.Core) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var name = c.Param("name")
-		var versionWithExt = c.Param("version")
-		var version = strings.Replace(versionWithExt, ".tgz", "", 1)
+func getPackageTarballHandler(core *registry.Core, c *gin.Context) (int, error) {
+	var name = c.Param("name")
+	var versionWithExt = c.Param("version")
+	var version = strings.Replace(versionWithExt, ".tgz", "", 1)
 
-		var tarball, err = core.GetPackageTarball(name, version)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		if len(tarball) == 0 {
-			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
-				"error": "not found",
-			})
-		} else {
-			c.Data(http.StatusOK, "application/octet-stream", tarball)
-		}
+	var tarball, err = core.GetPackageTarball(name, version)
+	if err != nil {
+		return http.StatusInternalServerError, err
 	}
+
+	if len(tarball) == 0 {
+		return http.StatusNotFound, fmt.Errorf("package not found")
+
+	}
+
+	c.Data(http.StatusOK, "application/octet-stream", tarball)
+	return 0, nil
 }
